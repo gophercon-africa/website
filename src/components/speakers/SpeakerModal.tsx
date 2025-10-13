0714995b03d75219ffe8cf5d139ac1d5c6b84875
nutcas3 <maurice.nyanja.e@gmail.com>
2025-10-14 01:22:30 +0300

    fix: remove center alignment from speaker modal social icons and image container

--- src/components/speakers/SpeakerModal.tsx
+++ src/components/speakers/SpeakerModal.tsx
@@ -23,7 +23,7 @@ const SpeakerModal: React.FC<SpeakerModalProps> = ({ speaker, isOpen, onClose })
       size="xl"
     >
       <div className="flex flex-col md:flex-row gap-6">
-        <div className="flex-shrink-0 flex flex-col items-center">
+        <div className="flex-shrink-0 flex flex-col">
           <Image
             src={speaker.imageUrl}
             alt={speaker.name}
@@ -32,7 +32,7 @@ const SpeakerModal: React.FC<SpeakerModalProps> = ({ speaker, isOpen, onClose })
             className="rounded-lg object-cover w-48 h-48 mb-4"
           />
           
-          <div className="flex gap-3 justify-center">
+          <div className="flex gap-3">
             {speaker.twitter && (
               <a
                 href={speaker.twitter}
